875dad4e3d47daad75e9a9f115063e01e1a1169f
HASSAN ARSHAD <hassanarshad115@gmail.com>
2017-10-29 00:31:17 -0700

    add class calculator reference

--- CalculatorAssigment/Program.cs
+++ CalculatorAssigment/Program.cs
@@ -16,7 +16,7 @@ namespace CalculatorAssigment
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new ClassCalculatorAssigment.Form2());
         }
     }
 }
